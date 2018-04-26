0ebd2b6c931e5dde275d8427e97bdf3e29f3725b
Ronald Kevin Burton <rkevinburton@charter.net>
2018-04-26 11:41:27 -0500

    Make Docker environment the same as Development

--- PocketNurse/Startup.cs
+++ PocketNurse/Startup.cs
@@ -85,7 +85,7 @@ namespace PocketNurse
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Docker"))
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
