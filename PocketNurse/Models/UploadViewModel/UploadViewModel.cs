e114da37a408398f013ac6178c18cfb8007fc79d
Kevin Burton <rkevinburton@charter.net>
2018-05-07 02:59:40 -0500

    Add view model for upload

---
+++ PocketNurse/Models/UploadViewModel/UploadViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace PocketNurse.Models.UploadViewModel
+{
+    // https://docs.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-2.1
+    public class UploadViewModel
+    {
+        IFormFile UploadFile { get; set; }
+    }
+}
