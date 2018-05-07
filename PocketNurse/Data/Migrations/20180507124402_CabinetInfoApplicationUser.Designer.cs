a90cbdc0d23901035b562483333ef240d72440a7
Ronald Kevin Burton <rkevinburton@charter.net>
2018-05-07 07:54:02 -0500

    Add meta-data to ApplicationUser

--- PocketNurse/Data/Migrations/20180507124402_CabinetInfoApplicationUser.Designer.cs
+++ PocketNurse/Data/Migrations/20180507124402_CabinetInfoApplicationUser.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PocketNurse.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180507124402_CabinetInfoApplicationUser")]
+    partial class CabinetInfoApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PocketNurse/Models/ApplicationUser.cs
+++ PocketNurse/Models/ApplicationUser.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PocketNurse.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(32)]
+        public string State { get; set; }
+        [StringLength(32)]
+        public string Area { get; set; }
     }
 }
